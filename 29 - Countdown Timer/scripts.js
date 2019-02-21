3f5a96d3f11b169d248db510662b98e5aa86f88d
newt <hi@newty.dev>
2019-02-21 22:07:00 +0000

    Day 29

---
+++ 29 - Countdown Timer/scripts.js
@@ -0,0 +1,59 @@
+let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
+
+function timer(seconds) {
+    clearInterval(countdown); // Clear any currently running countdowns
+
+    const now = Date.now();
+    const then = now + (seconds * 1000);
+
+    displayTimeLeft(seconds);
+    displayEndTime(then);
+
+    countdown = setInterval(() => {
+        const secondsLeft = Math.round((then - Date.now()) / 1000);
+
+        if (secondsLeft < 0) { // When the countdown finishes...
+            clearInterval(countdown); // ...stop running the setInterval...
+            return; // ...and return
+        }
+
+        displayTimeLeft(secondsLeft);
+    }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+
+    const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
+
+    document.title = display; // Set the window's title
+    timerDisplay.textContent = display;
+}
+
+function displayEndTime(timestamp) {
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const minutes = end.getMinutes();
+
+    endTime.textContent = `Be back at ${hour}:${minutes}`;
+}
+
+function startTimer() {
+    const seconds = parseInt(this.dataset.time);
+
+    timer(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
+
+document.customForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const secs = this.minutes.value * 60;
+    this.reset(); // Clear the textbox.
+    timer(secs);
+});
